15e2e24ff8d472ce5753f7b036d388d472231cee
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-01-12 13:44:21 +0100

    [wip]

--- alloc_lifetime.d
+++ alloc_lifetime.d
@@ -1,1 +1,3 @@
-sudo dtrace -n 'pid8504::xmalloc:return {self->lifetime[arg1]=timestamp} pid8504::xfree:entry {@duration = quantize((timestamp - self->lifetime[arg0])/1000000)}  tick-10s {exit(0)}' -p 8504
+#!/bin/sh -x
+
+dtrace -n 'pid$target::malloc:return {self->lifetime[arg1]=timestamp} pid$target::free:entry {@duration = quantize((timestamp - self->lifetime[arg0])/1000000)}  tick-10s {exit(0)}' $@
