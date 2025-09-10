5b491a9347665b5e42d6a0bb3bb09c0e76213208
Philippe Gaultier <philippe.gaultier@ory.sh>
2025-09-10 14:28:52 +0200

    print uuid argument in go sql query

--- go-sql-query.d
+++ go-sql-query.d
@@ -1,3 +1,4 @@
+
 typedef struct {
   uint8_t* ptr;
   size_t len;
@@ -11,34 +12,60 @@ typedef struct {
   uint8_t align;
   uint8_t field_align;
   uint8_t kind;
-} GoRtti;
+  void* equal_func;
+  uint8_t* gc_data;
+  int32_t name_offset;
+  int32_t ptr_to_this;
+} GoType;
 
 typedef struct {
-  GoRtti* rtti;
+  GoType* rtti;
   void* ptr;
 } GoInterface;
 
+
+typedef struct {
+  GoType type;
+  GoType* elem;
+  GoType* slice;
+  uintptr_t len;
+} GoArrayType;
+
 pid$target::database?sql.*.QueryContext:entry {
-  this->query = stringof(copyin(arg3, arg4));
+  this->query = stringof(copyin(arg3, arg4)); // Query string.
   this->args_ptr = arg5;
   this->args_count = arg6;
   printf("%p %d\n", arg5, arg6);
 
   printf("%s\n", this->query);
   this->iface0 = (GoInterface*) copyin(this->args_ptr, sizeof(GoInterface));
-  this->rtti0 = (GoRtti*) copyin((user_addr_t)this->iface0->rtti, sizeof(GoRtti));
+  print(*(this->iface0));
+  this->rtti0 = (GoType*) copyin((user_addr_t)this->iface0->rtti, sizeof(GoType));
   print(*(this->rtti0));
 
+  this->go_arr0 = (GoArrayType*)copyin((user_addr_t)this->iface0->rtti, sizeof(GoArrayType));
+  print(*(this->go_arr0));
+  this->go_arr0_elem = (GoType*)copyin((user_addr_t)this->go_arr0->elem, sizeof(GoType));
+  print(*(this->go_arr0_elem));
+  this->go_arr0_slice = (GoType*)copyin((user_addr_t)this->go_arr0->slice, sizeof(GoType));
+  print(*(this->go_arr0_slice));
+
+  this->go_str0 = (uint8_t*)copyin((user_addr_t)this->iface0->ptr, 16);
+  tracemem(this->go_str0, 16);
+
   this->iface1 = (GoInterface*) copyin(this->args_ptr + 1*sizeof(GoInterface), sizeof(GoInterface));
-  this->rtti1 = (GoRtti*) copyin((user_addr_t)this->iface1->rtti, sizeof(GoRtti));
+  print(*(this->iface1));
+  this->rtti1 = (GoType*) copyin((user_addr_t)this->iface1->rtti, sizeof(GoType));
   print(*(this->rtti1));
 
   this->iface2 = (GoInterface*) copyin(this->args_ptr + 2*sizeof(GoInterface), sizeof(GoInterface));
-  this->rtti2 = (GoRtti*) copyin((user_addr_t)this->iface2->rtti, sizeof(GoRtti));
+  print(*(this->iface2));
+  this->rtti2 = (GoType*) copyin((user_addr_t)this->iface2->rtti, sizeof(GoType));
   print(*(this->rtti2));
 
   this->iface3 = (GoInterface*) copyin(this->args_ptr + 3*sizeof(GoInterface), sizeof(GoInterface));
-  this->rtti3 = (GoRtti*) copyin((user_addr_t)this->iface3->rtti, sizeof(GoRtti));
+  print(*(this->iface3));
+  this->rtti3 = (GoType*) copyin((user_addr_t)this->iface3->rtti, sizeof(GoType));
   print(*(this->rtti3));
   printf("\n");
 
