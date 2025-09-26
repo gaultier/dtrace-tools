95d4dccab0867b188644e943fc7b361843daa704
Philippe Gaultier <philippe.gaultier@ory.sh>
2025-09-26 10:09:13 +0200

    [wip]

--- net-sockets.d
+++ net-sockets.d
@@ -1,15 +1,27 @@
 //#pragma D option quiet
 #pragma D option strsize=16K
 
+syscall::socket:return, syscall::listen:return
+/ pid == $target && arg0 != -1 /
+{
+  socket_fds[arg0] = 1;
+}
+
+syscall::close:entry
+/ pid == $target && socket_fds[arg0] != 0 /
+{
+  socket_fds[arg0] = 0;
+}
+
 syscall::write:entry, syscall::sendto_nocancel:entry, syscall::sendto:entry 
-/ pid == $target && arg0>2 && arg1 != 0 && arg2 >16 /
+/ pid == $target && socket_fds[arg0] != 0 && arg1 != 0 /
 {
    printf("action=write pid=%d execname=%s size=%d fd=%d: %s\n", pid, execname, arg2, arg0, stringof(copyin(arg1, arg2)));
 }
 
 
 syscall::read:entry, syscall::recvfrom_nocancel:entry, syscall::recvfrom:entry 
-/ pid == $target /
+/ pid == $target && socket_fds[arg0] != 0 /
 {
   self->read_ptr = arg1;
   self->read_fd = arg0;
